a65b70726f4cbabe154097856570acd60c73f05a
Семён <semeneroshevich@yandex.ru>
2026-07-22 19:41:59 +0700

    FIX

--- backend/api/views.py
+++ backend/api/views.py
@@ -20,7 +20,6 @@ from api.serializers import (
     FavoriteSerializer,
     IngredientSerializer,
     RecipeCreateUpdateSerializer,
-    RecipeMinifiedSerializer,
     RecipeReadSerializer,
     SetAvatarSerializer,
     ShoppingCartSerializer,
